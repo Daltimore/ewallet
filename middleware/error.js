127ea948d17f52a5cb1380d6a7a03b8f0dca2d7f
Faith Odonghanro <toriboi.fo@gmail.com>
2019-01-27 16:31:33 +0100

    Use express-async-errors

--- middleware/error.js
+++ middleware/error.js
@@ -1,6 +1,10 @@
+const createError = require('http-errors');
+
 module.exports = function (err, _, res) {
-  res.status(err.status || 500).json({
-    'status': err.status,
-    'message': err.message
+  const error = new createError.InternalServerError();
+
+  res.status(err.status || error.status || 500).json({
+    'status': err.status || error.status,
+    'message': err.message || error.message
   });
 }
